4c4871f1a253a91b4a965049087ac057373fd84f
Rustum Goden <rustum.goden@gmail.com>
2025-08-30 09:31:25 +0800

    fix editor user cant login

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
@@ -27,12 +28,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    // protected function password(): Attribute
-    // {
-    //     return Attribute::make(
-    //         set: fn ($value) => bcrypt($value),
-    //     );
-    // }
+    // Use a setter method instead of Attribute
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
 
     public function isAdmin()
     {
